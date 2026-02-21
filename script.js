954b61c6f22a1c18a90573ff44d1ada318929448
AlexCuadros <cuadrosal88@gmail.com>
2026-02-20 21:08:10 -0700

    Removed About Section and added Letters Section

--- script.js
+++ script.js
@@ -94,6 +94,35 @@
             }
         }
     });
+
+    // Gallery carousel
+    $(".letters-carousel").owlCarousel({
+        autoplay: false,
+        smartSpeed: 1500,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:2
+            },
+            768:{
+                items:3
+            },
+            992:{
+                items:4
+            },
+            1200:{
+                items:5
+            }
+        }
+    });
     
 })(jQuery);
-
